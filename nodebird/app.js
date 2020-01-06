dc005f5f2fc0240aabfcf1c5283b92f56af11d4f
yeji <yeji9175@naver.com>
2020-01-07 00:44:55 +0900

    2020-01-07
    SNS 서비스 만들기 수정

--- nodebird/app.js
+++ nodebird/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
-const morgan = require('morgan');
+const morgan = require('morgan'); // 로깅 모듈
 const path = require('path');
 const session = require('express-session');
 const flash = require('connect-flash');
@@ -10,41 +10,41 @@ const pageRouter = require('./routes/page');
 
 const app = express();
 
-app.set('views', path.join(__dirname,'views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.set('port', process.env.PORT || 8001);
 
-app.use(morgan('dev')); // morgan : 로깅 모듈
+app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.json);
-app.use(express.urlencoded({extended: false})); // false 면 querystring || true 면 querystring 확장모듈 qs 사용
-app.use(cookieParser(process.env.COOKIE_SECRET)); // .env 파일에서 불러옴
+app.use(express.json());
+app.use(express.urlencoded({ extended: false })); // false 면 querystring || true 면 querystring 확장모듈 qs 사용
+app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(session({
-    resave: false,
-    saveUninitialized: false,
-    secret: 'nodebirdsecret',
-    cookie: {
-        httpOnly: true,
-        secure: false,
-    },
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true,
+    secure: false,
+  },
 }));
 app.use(flash());
 
 app.use('/', pageRouter);
 
 app.use((req, res, next) => {
-   const err = new Error('Not Found');
-   err.status = 404;
-   next(err);
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 app.use((err, req, res, next) => {
-   res.locals.message = err.message;
-   res.locals.error = req.app.get('env') === 'development' ? err : {};
-   res.status(err.status || 500);
-   res.render('error');
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 app.listen(app.get('port'), () => {
-    console.log(app.get('port'), '번 포트에서 대기 중');
+  console.log(app.get('port'), '번 포트에서 대기중');
 });
