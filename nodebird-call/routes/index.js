9fa323fe4697e975e1e6893a8558dcacc9d6d5f7
yeji <yeji9175@naver.com>
2020-06-20 03:50:35 +0900

    2020-06-20
    SNS API 서버 만들기

--- nodebird-call/routes/index.js
+++ nodebird-call/routes/index.js
@@ -2,29 +2,60 @@ const express = require('express');
 const axios = require('axios');
 
 const router = express.Router();
+const URL = 'http://localhost:8002/v1';
+
+axios.defaults.headers.origin = 'http://localhost:8003';
+// NOTE: 토큰을 발급받는 부분을 함수로 만들어 재사용
+const request = async (req, api) => {
+  try {
+    if (!req.session.jwt) { // 세션에 토큰이 없으면
+      const tokenResult = await axios.post(`${URL}/token`, {
+        clientSecret: process.env.CLIENT_SECRET,
+      });
+      req.session.jwt = tokenResult.data.token; // 세션에 토큰 저장
+    }
+    return await axios.get(`${URL}${api}`, {
+      headers: { authorization: req.session.jwt },
+    }); // API 요청
+  } catch (error) {
+    console.error(error);
+    if (error.response.status < 500) { // 410이나 419처럼 의도된 에러면 발생
+      return error.response;
+    }
+    throw error;
+  }
+};
+
+router.get('/mypost', async (req, res, next) => {
+  try {
+    const result = await request(req, '/posts/my');
+    res.json(result.data);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.get('/search/:hashtag', async (req, res, next) => {
+  try {
+    const result = await request(
+        req, `/posts/hashtag/${encodeURIComponent(req.params.hashtag)}`,
+        );
+    res.json(result.data);
+  } catch (error) {
+    if (error.code) {
+      console.error(error);
+      next(error);
+    }
+  }
+});
 
 router.get('/test', async (req, res, next) => {
   try {
-    if (!req.session.jwt) { // 세션에 토큰이 없으면
-      const tokenResult = await axios.post('http://localhost:8002/v1/token', {
-        clientSecret: process.env.CLIENT_SECRET, // uuidv4로 생성(범용 고유 식별자)
-      });
-      if (tokenResult.data && tokenResult.data.code === 200) { // 토큰 발급 성공
-        req.session.jwt = tokenResult.data.token; // 세션에 토큰 저장
-      } else { // 토큰 발급 실패
-        return res.json(tokenResult.data); // 발급 실패 사유 응답
-      }
-    }
-    // 발급받은 토큰 테스트
-    const result = await axios.get('http://localhost:8002/v1/test', {
-      headers: { authorization: req.session.jwt },
-    });
+    const result = await request(req, `${URL}/test`);
     return res.json(result.data);
   } catch (error) {
     console.error(error);
-    if (error.response.status === 419) { // 토큰 만료 시
-      return res.json(error.response.data);
-    }
     return next(error);
   }
 });
