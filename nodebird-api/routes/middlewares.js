84e1ab97460a29e9d752a0d53405e71ba9fa705d
yeji <yeji9175@naver.com>
2020-06-23 03:04:18 +0900

    2020-06-23
    사용량 제한 구현하기

--- nodebird-api/routes/middlewares.js
+++ nodebird-api/routes/middlewares.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const RateLimit = require('express-rate-limit');
 
 exports.isLoggedIn = (req, res, next) => {
     if (req.isAuthenticated()) {
@@ -32,4 +33,28 @@ exports.verifyToken = (req, res, next) => {
             message: '유효하지 않은 토큰입니다',
         });
     }
+};
+
+exports.apiLimiter = new RateLimit({
+    // NOTE: 기준 시간
+    windowMs: 60 * 1000, //1분
+    // NOTE: 허용 횟수
+    max: 1,
+    // NOTE: 호출 간격
+    delayMs: 0,
+    // NOTE: 제한 초과 시 콜백 함수
+    handler(req, res) {
+        res.status(this.statusCode).json({
+            code: this.statusCode, // 기본값 429
+            message: '1분에 한 번만 요청할 수 있습니다.',
+        });
+    },
+});
+
+// 사용하면 안 되는 라우터에 붙임
+exports.deprecated = (req, res) => {
+    res.status(410).json({
+        code: 410,
+        message: '새로운 버전이 나왔습니다. 새로운 버전을 사용하세요.',
+    });
 };
--- nodebird-api/routes/v2.js
+++ nodebird-api/routes/v2.js
@@ -1,20 +1,15 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 
-const { verifyToken, deprecated } = require('./middlewares');
+const { verifyToken, apiLimiter } = require('./middlewares');
 const { Domain, User, Post, Hashtag } = require('../models');
 
 const router = express.Router();
 
-//NOTE: v1으로 접근한 모든 요청에 deprecated 응답을 보냄
-//NOTE: 실제 서비스 운영 시에는 v2가 나왔다고 바로 v1을 닫아 버리기보다는 일정한 기간을 두고 옮겨가는 것이 좋음.
-//NOTE: 사용자가 변경된 부분을 자신의 코드에 반영할 시간이 필요하기 때문.
-router.use(deprecated());
-
-router.post('/token', async (req, res) => {
+router.post('/token', apiLimiter, async (req, res) => {
     const { clientSecret } = req.body;
     try {
-        const domain = await Domain.findOne({
+        const domain = await Domain.find({
             where: { clientSecret },
             include: {
                 model: User,
@@ -27,15 +22,12 @@ router.post('/token', async (req, res) => {
                 message: '등록되지 않은 도메인입니다. 먼저 도메인을 등록하세요',
             });
         }
-        //NOTE: 첫번째 인자 - 토큰의 내용
-        //NOTE: 두번째 인자 - 토큰의 비밀키
-        //NOTE: 세번째 인자 - 토큰의 설정
         const token = jwt.sign({
             id: domain.user.id,
             nick: domain.user.nick,
         }, process.env.JWT_SECRET, {
-            expiresIn: '1m', // 유효기간: 1분
-            issuer: 'nodebird', // 발급자
+            expiresIn: '30m', // 30분
+            issuer: 'nodebird',
         });
         return res.json({
             code: 200,
@@ -51,12 +43,11 @@ router.post('/token', async (req, res) => {
     }
 });
 
-router.get('/test', verifyToken, (req, res) => {
-    // req.decoded : jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
+router.get('/test', verifyToken, apiLimiter, (req, res) => {
     res.json(req.decoded);
 });
 
-router.get('/posts/my', verifyToken, (req, res) => {
+router.get('/posts/my', apiLimiter, verifyToken, (req, res) => {
     Post.findAll({ where: { userId: req.decoded.id } })
         .then((posts) => {
             console.log(posts);
@@ -74,7 +65,7 @@ router.get('/posts/my', verifyToken, (req, res) => {
         });
 });
 
-router.get('/posts/hashtag/:title', verifyToken, async (req, res) => {
+router.get('/posts/hashtag/:title', verifyToken, apiLimiter, async (req, res) => {
     try {
         const hashtag = await Hashtag.find({ where: { title: req.params.title } });
         if (!hashtag) {
